31f777935e10e082ad69099e50959f6ba62b6761
a15elibr <a15elibr@student.his.se>
2018-03-08 15:59:02 +0100

    added a mini version

--- testning/migwiz1/app.js
+++ testning/migwiz1/app.js
@@ -8,6 +8,7 @@ var mysql = require('mysql');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var mysql = require('./routes/mysql');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/mysql', mysql);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
