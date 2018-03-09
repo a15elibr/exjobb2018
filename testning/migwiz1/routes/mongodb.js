d40f3d89b6a8309334a08f9cca06f5e55c6570e8
a15elibr <a15elibr@student.his.se>
2018-03-09 15:15:16 +0100

    fixed mongodb connection

--- testning/migwiz1/routes/mongodb.js
+++ testning/migwiz1/routes/mongodb.js
@@ -8,28 +8,22 @@ router.get('/', function(req, res, next) {
     var personList = [];
     var url = 'mongodb://localhost:27017/';
     mongodb.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("test");
-        dbo.collection("people").find({}).toArray(function(err, result) {
-            if (err) {
-                res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-            } else {
-                // Loop check on each row
-                for (var i = 0; i < result.length; i++) {
-
-                    // Create an object to save current row's data
-                    var person = {
-                        'ID':result[i].ID,
-                        'name':result[i].name
-                    }
-                    // Add object into array
-                    personList.push(person);
-                }
-            }
-            db.close();
-        });
-        
-        res.render('mongodb', { personList: personList });
+      if (err) throw err;
+      var dbo = db.db("test");
+      dbo.collection("people").find({}).toArray(function(err, result) {
+        if (err) {
+            throw err;
+            console.log('throw err');
+        } else if (result.length){
+            res.render('mongodb', { personList: result });
+            console.log('I found some data');
+            
+        } else {
+            console.log('I found no data');
+            res.render('mongodb', { });
+        }
+        db.close();
+      });
     });
 
 });
