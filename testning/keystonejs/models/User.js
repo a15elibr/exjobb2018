2bc4f4c3e78bfac848206c895157405417b38c3b
a15elibr <a15elibr@student.his.se>
2018-03-17 14:59:43 +0100

    reworked subdomain into user

--- testning/keystonejs/models/User.js
+++ testning/keystonejs/models/User.js
@@ -5,7 +5,8 @@ var Types = keystone.Field.Types;
  * User Model
  * ==========
  */
-var User = new keystone.List('User');
+var User = new keystone.List('User', {
+});
 
 User.add({
 	name: { type: Types.Name, required: true, index: true },
@@ -13,9 +14,11 @@ User.add({
 	password: { type: Types.Password, initial: true, required: true },
     username: { type: Types.Text, required: false, initial: true, unique: false },
     regdate: { type: Types.Date, required: false, initial: true },
-    group: { type: Types.Select, options: 'admin, teacher, student', default: 'student' }
+    group: { type: Types.Select, options: 'admin, teacher, student', default: 'student' },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+}, 'Subdomain', {
+    subname: { type: Types.Text, initial: true },
 });
 
 // Provide access to Keystone
@@ -24,12 +27,11 @@ User.schema.virtual('canAccessKeystone').get(function () {
 });
 
 
+
 /**
  * Relationships
  */
 User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
-User.relationship({ ref: 'Subdomain', refPath: 'username' });
-
 /**
  * Registration
  */
@@ -37,5 +39,7 @@ User.relationship({ ref: 'Subdomain', refPath: 'username' });
 // bugfix ? 
 // exports = module.exports = User;
 
-User.defaultColumns = 'name, email, isAdmin, username, regdate';
+User.defaultColumns = 'name, email, isAdmin, username, regdate, group, subdomain';
 User.register();
+
+
