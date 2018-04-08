aac1e4eb64620ef95a5db0ab2954d0af8e629e5f
a15elibr <a15elibr@student.his.se>
2018-04-08 14:15:57 +0200

    set post model to fit wordpress posts

--- keystonejs/models/Post.js
+++ keystonejs/models/Post.js
@@ -16,7 +16,9 @@ Post.add({
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+    modifiedDate: { type: Types.Date },
 	image: { type: Types.CloudinaryImage },
+    type: { type: Types.Select, options: 'post, page', default: 'post' },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 },
