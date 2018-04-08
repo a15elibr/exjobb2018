415578ca936680349b4a0b13fd34bc4fcbb5e615
a15elibr <a15elibr@student.his.se>
2018-04-08 17:00:17 +0200

    added query tests

---
+++ migwiz/models/User.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+    
+    // via mongoose
+    mongoose.connect('mongoDB://localhost:27017/keystone', function(){
+        console.log('connected');
+    });
+    var Schema = mongoose.Schema;
+
+    // create a schema
+    var userSchema = new Schema({
+        name: {
+            first: String,
+            last: String
+        },
+        username: String,
+        wp_id: Number
+    });
+
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = User;
