ca32b707ab4ca1158a79177c0a735fcaf3cf9bac
a15elibr <a15elibr@student.his.se>
2018-04-13 17:37:47 +0200

    now uses correct user prefix

--- misc/tampermonkey/add-site.js
+++ misc/tampermonkey/add-site.js
@@ -4,31 +4,32 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.kaermorhen.r4t/wp-admin/network/site-new*
+// @match        http://www.wordpress.lc/wp-admin/network/site-new*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    // Use local storage to keep count of added users
     var counter = localStorage.getItem("counter");
     var userPrefix = "student";
-
+    
+    // Add 1000 users
     if(counter < 1000){
+        // add values
+        document.getElementById('site-address').value = userPrefix+counter;
+        document.getElementById('site-title').value = "Student blog";
+        document.getElementById('admin-email').value = userPrefix+counter + "@student.wordpress.lc";
 
-    // add values
-    document.getElementById('site-address').value = userPrefix+counter;
-    document.getElementById('site-title').value = "Student blog";
-    document.getElementById('admin-email').value = userPrefix+counter + "@kaermorhen.r4t";
+        // incr coutner
+        counter++;
+        localStorage.setItem("counter", counter);
 
-    // incr coutner
-    counter++;
-    localStorage.setItem("counter", counter);
-
-    // submit
-    document.querySelector('form').submit();
+        // submit
+        document.querySelector("form").submit();
     } else {
         alert("No more!");
     }
     
-})();
+});
