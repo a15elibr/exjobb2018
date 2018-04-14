907c0fdf1420d76f6bf364686ad762529f09a606
a15elibr <a15elibr@student.his.se>
2018-04-14 21:02:02 +0200

    added activation key

--- keystonejs/models/User.js
+++ keystonejs/models/User.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var Email = require('keystone-email');
 var Types = keystone.Field.Types;
 
 /**
@@ -19,6 +20,9 @@ User.add({
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 },  'Subdomain', {
     subname: { type: Types.Text, initial: true },
+},  'Activation key', {
+    activation_key: { type: String, initial: false, required: false },
+    isKeyActive: { type: Boolean },
 });
 
 // Provide access to Keystone
@@ -26,12 +30,38 @@ User.schema.virtual('canAccessKeystone').get(function () {
 	return this.isAdmin;
 });
 
+// -- Send email to a new user with activiation key 
+User.schema.pre('save', function(next) {
+    if(this.isKeyActive){
+            new Email('./templates/emails/email.pug', {
+            transport: 'mailgun',
+        }).send({
+            username: this.username,
+            activation_key: this.activation_key,
+        }, {
+            apiKey: 'key-d94c30fc9c5bfa9a0a115c1e63fb2de0',
+            domain: 'sandboxe93479b36ba5496bb2f1f69131955a2b.mailgun.org',
+            to: this.email,
+            from: {
+                name: 'Your Site',
+                email: 'hello@yoursite.com',
+            },
+            subject: 'Welcome to KeystoneJS',
+        }, function (err, result) {
+            if (err) {
+                console.error('Mailgun test failed with error:\n', err);
+            } else {
+                console.log('Successfully sent Mailgun test with result:\n', result);
+            }
+        });
+    }
+    next();
+});
 
 /**
  * Relationships
  */
 User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
-
 
 /**
  * Registration
